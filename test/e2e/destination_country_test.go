abdcf55806c3ce3d7b284b0e8617232fa16556ad
aibudaev <aibudaevv@gmail.com>
2026-07-24 16:34:43 +0300

    test(e2e): [S14-9.T04] add metricWithLabelExists guards, use float64(callCount) in destination_country

--- test/e2e/destination_country_test.go
+++ test/e2e/destination_country_test.go
@@ -63,20 +63,25 @@ func TestDestinationCountry(t *testing.T) {
 
 			env := newTestEnvWithExtraEnv(ctx, t, "", extraEnv)
 
-			runSippScenario(ctx, t, "uas_100.xml", tt.uacScenario, 100, env)
+			const callCount = 100
+			runSippScenario(ctx, t, "uas_100.xml", tt.uacScenario, callCount, env)
 
 			wantLabel := `destination_country="` + tt.wantCountry + `"`
 
+			require.True(t, metricWithLabelExists(t, env.endpoint, "sip_exporter_invite_total", wantLabel),
+				"invite_total{%s} must exist", wantLabel)
 			inviteOK := getMetricWithLabel(t, env.endpoint, "sip_exporter_invite_total", wantLabel)
 			inviteBad := getMetricWithLabel(t, env.endpoint, "sip_exporter_invite_total", tt.notWantLabel)
 			t.Logf("invite_total{%s}=%.0f, invite_total{%s}=%.0f", wantLabel, inviteOK, tt.notWantLabel, inviteBad)
-			require.InDelta(t, 100.0, inviteOK, ratioDelta, "invite_total should carry %s", wantLabel)
+			require.InDelta(t, float64(callCount), inviteOK, ratioDelta, "invite_total should carry %s", wantLabel)
 			require.LessOrEqual(t, inviteBad, 3.0, "invite_total should NOT carry %s", tt.notWantLabel)
 
+			require.True(t, metricWithLabelExists(t, env.endpoint, "sip_exporter_invite_200_total", wantLabel),
+				"invite_200_total{%s} must exist", wantLabel)
 			invite200OK := getMetricWithLabel(t, env.endpoint, "sip_exporter_invite_200_total", wantLabel)
 			invite200Bad := getMetricWithLabel(t, env.endpoint, "sip_exporter_invite_200_total", tt.notWantLabel)
 			t.Logf("invite_200_total{%s}=%.0f, invite_200_total{%s}=%.0f", wantLabel, invite200OK, tt.notWantLabel, invite200Bad)
-			require.InDelta(t, 100.0, invite200OK, ratioDelta, "invite_200_total should carry %s", wantLabel)
+			require.InDelta(t, float64(callCount), invite200OK, ratioDelta, "invite_200_total should carry %s", wantLabel)
 			require.LessOrEqual(t, invite200Bad, 3.0, "invite_200_total should NOT carry %s", tt.notWantLabel)
 
 			waitForSessionsZero(t, env.endpoint)
